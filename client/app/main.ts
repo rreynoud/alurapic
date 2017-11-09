b1d95de73609fba12b6430c544e4162a8424a5cc
Rafael <rreynoud@gmail.com>
2017-11-09 07:11:07 -0200

    Finalizado primeira parte

---
+++ client/app/main.ts
@@ -0,0 +1,6 @@
+// alurapic/client/app/main.ts
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module'; // importa o módulo que será carregado primeiro
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
