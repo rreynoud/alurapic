a2f0289c839bf771caaffabde8dfdbcf5b37eb8a
Rafael <rreynoud@gmail.com>
2017-11-18 15:49:41 -0200

    Funcionando a parte de rotas

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -9,10 +9,11 @@ import { HttpModule } from '@angular/http';
 import { PainelModule } from './painel/painel.module';
 import { CadastroComponent }   from './cadastro/cadastro.component'; 
 import { ListagemComponent }   from './listagem/listagem.component'; 
+import { routing } from './app.routes';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule ],
+  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule, routing ],
   declarations: [ AppComponent , CadastroComponent, ListagemComponent],
   bootstrap:    [ AppComponent ]
 })
