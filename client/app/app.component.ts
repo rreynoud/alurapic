f5755256687e9b47a3dd82f274fde3bbde8b1545
Rafael <rreynoud@gmail.com>
2017-11-18 14:02:46 -0200

    Funcionando tudo - final aula 3

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -16,11 +16,9 @@ export class AppComponent {
         console.log('Construindo AppComponent');
 
         http.get('v1/fotos').map(res => res.json())
-        .subscribe(
-            fotos => this.fotos = fotos,
-            erro => console.log(erro)
-        );
-        
-        
+        .subscribe(fotos => {
+            this.fotos = fotos;
+            console.log(this.fotos);
+        },erro => console.log(erro));        
     }    
 }
