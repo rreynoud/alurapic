c92a87f2b8d4d88705436ea7b45201bdcc41ed68
Rafael <rreynoud@gmail.com>
2017-11-18 14:17:45 -0200

    Criado novo componente de painel e aula ate a mensagem Socorro, meu painel nada exibe!

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -6,9 +6,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { FotoModule } from './foto/foto.module';
 import { HttpModule } from '@angular/http';
+import { PainelModule } from './painel/painel.module';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FotoModule, HttpModule ],
+  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
