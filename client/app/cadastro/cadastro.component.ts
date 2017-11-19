58afdc9c8dc947b95d4f9fb18e3758a2b82a3da7
Rafael <rreynoud@gmail.com>
2017-11-19 09:10:36 -0200

    Funcionando tudo com ngModel

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -11,9 +11,16 @@ export class CadastroComponent{
 
     foto: FotoComponent = new FotoComponent();
 
+    constructor(){
+        this.foto.descricao = 'a';
+        this.foto.titulo = 't';
+        this.foto.url = 'u'
+    }
+
     cadastrar(event){
         event.preventDefault();
         console.log(this.foto);
+        
     }
 
     
